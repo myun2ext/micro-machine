f8749068d58fe4efaf9504bd72a6e6ce613c09fe
myun2ext <myun2@mbm.nifty.com>
2014-08-12 17:29:41 +0900

    Implement stack machine Main!!

--- stack_machine.c
+++ stack_machine.c
@@ -1,28 +1,42 @@
 #include <stdio.h>
 
-/*struct sm_stack
+struct sm_stack
 {
 	char buffer[2048];
-};*/
-
+	char* top_ptr;
+};
 char script_buffer[1024 * 16];
 
-const char* read_script(const char* filename) {
+const char* read_script(const char* filename)
+{
 	FILE *fp = fopen(filename, "r");
 	if ( fp == NULL )
 		return NULL;
-	fread(script_buffer, sizeof(script_buffer), fp);
+
+	fread(script_buffer, sizeof(script_buffer), 1, fp);
 	return script_buffer;
 }
 
+int run(struct sm_stack*, const char* readed_buffer, char* script_ptr);
+
 int main(int argc, const char *argv[])
 {
+	/*  Variables */
 	const char* script_filename = argv[1];
-	char buffer[256];
-	char stack_buffer[2048];
+	char readed[256];
+	struct sm_stack stack = { "", NULL };
 	int ret = 0;
-	while(fgets(buffer, sizeof(buffer), stdin) != 0) {
-		//ret = run(stack_buffer, buffer, 
+	const char* script_ptr = script_buffer;
+
+	/*  Init stack */
+	stack.top_ptr = stack.buffer;
+
+	/*  Read Script */
+	read_script(script_filename);
+
+	/*  Running... */
+	while(fgets(readed, sizeof(readed), stdin) != 0) {
+		ret = run(&stack, readed, &script_ptr);
 	}
 	return ret;
 }
