7d0566f5045b43beac606325f7a7443cc3c4652b
myun2ext <myun2@mbm.nifty.com>
2014-08-12 17:20:15 +0900

    implementings...

--- stack_machine.c
+++ stack_machine.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+/*struct sm_stack
 {
-	const char* program_filename = argv[1];
+	char buffer[2048];
+};*/
+
+char script_buffer[1024 * 16];
+
+const char* read_script(const char* filename) {
+	FILE *fp = fopen(filename, "r");
+	if ( fp == NULL )
+		return NULL;
+	fread(script_buffer, sizeof(script_buffer), fp);
+	return script_buffer;
+}
+
+int main(int argc, const char *argv[])
+{
+	const char* script_filename = argv[1];
+	char buffer[256];
+	char stack_buffer[2048];
+	int ret = 0;
+	while(fgets(buffer, sizeof(buffer), stdin) != 0) {
+		//ret = run(stack_buffer, buffer, 
+	}
+	return ret;
 }
