e20d0828fd7dbfb329e74aa75f5b809d2938beab
myun2ext <myun2@mbm.nifty.com>
2014-08-12 18:38:33 +0900

    multi proc [], skip \r \n

--- machine.c
+++ machine.c
@@ -10,12 +10,35 @@ struct machine
 	char* top_ptr;
 };
 
+int proc_script(struct machine* m, char c)
+{
+	if ( *(m->script_current) == 'p' )
+		putchar(c);
+	return 0;
+}
+
 int proc_byte(struct machine* m, char c)
 {
 	if ( c == '\0' )
 		return 1;
 
-	putchar(c);
+	/*  Skip Return */
+	while(*(m->script_current) == '\r' || *(m->script_current) == '\n' )
+		m->script_current++;
+
+	if ( *(m->script_current) == '\0' )
+		m->script_current = m->script;
+		//return 1;
+
+	if ( *(m->script_current) == '[' ) {
+		while(*(m->script_current++) != ']') {
+			proc_script(m,c);
+		}
+	}
+	else
+		proc_script(m,c);
+
+	m->script_current++;
 	return 0;
 }
 
