5ba073aec9cc5c691034e55bd7d040c9e7f53f9f
myun2 <myun2@nwhite.info>
2014-08-18 17:05:26 +0900

    struct machine.h

--- header/machine.h
+++ header/machine.h
@@ -1,4 +1,8 @@
 #ifndef __MYUN2_GITHUB__MICRO_MACHINE__MACHINE_H__
 #define __MYUN2_GITHUB__MICRO_MACHINE__MACHINE_H__
 
+typedef struct {
+
+} mm_machine;
+
 #endif//__MYUN2_GITHUB__MICRO_MACHINE__MACHINE_H__
