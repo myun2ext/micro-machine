876dcb5968c9633d431fc3e4ecdd9aeec2a1c04b
myun2 <myun2@nwhite.info>
2014-08-18 17:07:35 +0900

    machine of stacks

--- header/machine.h
+++ header/machine.h
@@ -1,8 +1,14 @@
 #ifndef __MYUN2_GITHUB__MICRO_MACHINE__MACHINE_H__
 #define __MYUN2_GITHUB__MICRO_MACHINE__MACHINE_H__
 
-typedef struct {
+typedef int 			mm_data_t;
+typedef unsigned int 	mm_size_t;
+typedef mm_data_t 		mm_stack_entry_t;
 
+typedef struct {
+	mm_stack_entry_t	*stack;
+	mm_size_t			stack_size;
+	mm_size_t			stack_used;
 } mm_machine;
 
 #endif//__MYUN2_GITHUB__MICRO_MACHINE__MACHINE_H__
