817c6c2e55a631d643753e90bc816d3bf0e72357
myun2 <myun2@nwhite.info>
2014-08-18 18:11:01 +0900

    test count sum up

--- mm_test.c
+++ mm_test.c
@@ -5,16 +5,25 @@ int g_test_successfully = 0;
 
 void expect_int(const char* context, int x, int result)
 {
+  g_test_total++;
+
   if ( x != result )
 	printf("\n\nFailed %s. (expected %d, but result is %d)\n\n",
 		context, x, result);
-  else
+  else {
 	putchar('.');
+	g_test_successfully++;
+  }
 }
 
 int main()
 {
   expect_int("test1", 0, 0);
   expect_int("test2", 1, 0);
+
+  printf ("Finally\n");
+  printf ("  Total: %d, Successfully: %d, Error: %d\n\n",
+	  g_test_total, g_test_successfully, g_test_total - g_test_successfully);
+
   return 0;
 }
